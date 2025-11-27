3a33f1bfd0ea1b2f84024154fd6ab38a8fc9e588
marlonlagosios <asesorpn@gmail.com>
2025-11-27 14:27:34 -0600

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -36,13 +36,13 @@ export default async function Projects() {
             <div key={item._id} className="p-4 relative" >
               <div className="sm:w-[370px] h-[320px] relative">
                 <WixMediaImage
-                  media={item.Image}
-                  alt={item.Type}
+                  media={item.exampleImage}
+                  alt={item.category}
                   objectFit="cover"
                 />
               </div>
               <div className="bg-white sm:mt-[-50px] border-t-4 relative mx-6 px-2 pt-3 border-blue-site text-center">
-                <p className="text-sm mb-6">{item.Prompt}</p>
+                <p className="text-sm mb-6">{item.promptText}</p>
               </div>
             </div>
           ))}
